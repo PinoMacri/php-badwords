020366915c51cb10b12c3ebac0f5d77f3b6df5cc
PinoMacri <pinonegozio1@outlook.it>
2023-02-06 15:10:47 +0100

    Esercizio Consegnato

--- register.php
+++ register.php
@@ -1,3 +1,31 @@
 <?php
+$censured = $_GET["censura"];
+$paragraph = $_GET["paragrafo"];
 
+$contatore = strlen($paragraph);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <h2>Paragrafo originale </h2>
+        <p><?php echo $paragraph ?></p>
+        <p>Il paragrafo originale era lungo <?php echo $contatore ?> caratteri</p>
+    </div>
+
+    <div>
+        <h2>Paragrafo censurato</h2>
+        <p><?php echo str_replace(ucfirst($censured), "***", $paragraph) ?></p>
+        <p>Il paragrafo originale era lungo <?php echo $contatore ?> caratteri</p>
+    </div>
+</body>
+
+</html>
